d9f462b1e5d0121ec8122a3cc709bd6199f55e02
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-16 08:48:22 +0100

    get subset_def working

--- Game/Levels/SetWorld/L01_subset_refl.lean
+++ Game/Levels/SetWorld/L01_subset_refl.lean
@@ -27,6 +27,23 @@ TacticDoc exact
 
 NewTactic rw intro apply exact
 
+/--
+`subset_def` is the proof of `(S ⊆ T) ↔ ∀ x, x ∈ S → x ∈ T`.
+
+If you're working with subsets from first principles, then `rw [subset_def]`
+will change occurrences of `S ⊆ T` in the goal to `∀ x, x ∈ S → x ∈ T`.
+
+Variants:
+
+* `rw [subset_def] at h` (change the definition at hypothesis `h`)
+* `rw [subset_def] at *` (change the definition everywhere)
+-/
+TheoremDoc MySet.subset_def as "subset_def" in "Set"
+
+NewTheorem MySet.subset_def
+-- **TODO** warning ``Add `LemmaDoc MySet.subset_def` somewhere above this statement.``
+-- suggests deprecated LemmaDoc
+
 /-- Every set $S$ is a subset of itself. -/
 TheoremDoc MySet.subset_refl as "subset_refl" in "Set"
 
