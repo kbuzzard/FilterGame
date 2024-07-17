234b6ad52db01bfe3e6c3e4a60bc251757debd9a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-17 23:39:29 +0100

    filter level 1

---
+++ Game/Levels/FilterWorld.lean
@@ -0,0 +1,12 @@
+import Game.Levels.FilterWorld.L01_univ_mem_principal
+
+World "FilterWorld"
+Title "Filter World"
+
+Introduction "A filter on `𝓧` should be thought of as a *generalised subset*
+of `𝓧`.
+
+Every subset of `𝓧` gives you a filter on `𝓧`, and at the start of this world
+we're going to explain how this works.
+
+Click \"Start →\" to continue."
