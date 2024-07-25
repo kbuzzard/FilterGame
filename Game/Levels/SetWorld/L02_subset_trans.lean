142f0341ca882c62e6620bc7a4295ebd53b05674
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-25 09:02:47 +0100

    tidying up set level 2

--- Game/Levels/SetWorld/L02_subset_trans.lean
+++ Game/Levels/SetWorld/L02_subset_trans.lean
@@ -25,9 +25,10 @@ Statement subset_trans {S T U : Set 𝓧} (hST : S ⊆ T) (hTU : T ⊆ U) : S �
   Hint "Start with `rw [subset_def] at *` to replace all occurences of the `⊆` symbol with
     their definitions."
   rw [subset_def] at *
-  Hint "Now we've got to prove something for all `x`, so `intro x`."
-  intro x
-  Hint "Now we assume `x ∈ S` with `intro h`"
+  Hint "Now we've got to prove something for all elements of `𝓧`, so `intro a`
+  to let `a` be an arbitrary element of `𝓧`."
+  intro a
+  Hint "Now we assume `a ∈ S` with `intro h`"
   intro h
   Hint "Now we just have to put the pieces together.
     You can either argue forwards, with `apply hST at h`,
@@ -35,12 +36,12 @@ Statement subset_trans {S T U : Set 𝓧} (hST : S ⊆ T) (hTU : T ⊆ U) : S �
   Branch
     apply hST at h
     Hint "Now figure out how to use `hTU` to finish things off."
-    Hint (hidden := true) "`apply hTU at h` or `apply hTU` make progress. Then `exact h`"
+    Hint (hidden := true) "`apply hTU at h` or `apply hTU` make progress. Then `exact h`."
     apply hTU at h
     exact h
   apply hTU
   Hint "Now figure out how to use `hST` to finish things off."
-  Hint (hidden := true) "`apply hST at h` or `apply hST` make progress. Then `exact h`"
+  Hint (hidden := true) "`apply hST at h` or `apply hST` make progress. Then `exact h`."
   exact hST _ h
 
-Conclusion "Nice! Let's now prove another basic property of subsets."
+Conclusion "Nice! Let's now prove antisymmetry, the third and final basic property of subsets."
