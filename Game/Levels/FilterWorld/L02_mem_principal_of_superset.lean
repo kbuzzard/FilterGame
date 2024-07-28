2b7a789f5c5ac7e898d17d3198988d0bf2c78830
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-28 21:42:49 +0100

    filter level 2 clarification.

--- Game/Levels/FilterWorld/L02_mem_principal_of_superset.lean
+++ Game/Levels/FilterWorld/L02_mem_principal_of_superset.lean
@@ -6,7 +6,8 @@ Level 2
 Title "Supersets and filters."
 
 Introduction "In this level, we'll show that if `S ∈ 𝓟 A` then any
-bigger set `T ⊇ S` is also in `𝓟 A`."
+bigger set `T ⊇ S` is also in `𝓟 A`. Remember that `𝓟 A` is the
+collection of all subsets of `𝓧` which contain `A`."
 
 namespace MyGame
 
@@ -20,7 +21,8 @@ open Filter
 Statement {A S T : Set 𝓧} (hAS : S ∈ 𝓟 A) (hST : S ⊆ T) : T ∈ 𝓟 A := by
   Hint "Start with `rw [mem_principal] at *` to remove all mention of `𝓟`."
   rw [mem_principal] at *
-  Hint "Now you can finish with `exact subset_trans hAS hST`"
+  Hint "Now you can finish with `exact subset_trans hAS hST`. You can think of `subset_trans` as
+  a *function* which takes two proofs as input, and returns a proof as output. "
   exact subset_trans hAS hST
 
 Conclusion "Now let's show a fact about `𝓟 A` and intersections."
