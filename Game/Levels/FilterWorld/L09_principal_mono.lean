d9213525b3a0724d2cc10663799af5bb340742c6
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-29 18:43:36 +0100

    filter level 9

--- Game/Levels/FilterWorld/L09_principal_mono.lean
+++ Game/Levels/FilterWorld/L09_principal_mono.lean
@@ -1,4 +1,4 @@
-import Game.Levels.FilterWorld.L08_le_principal
+import Game.Levels.FilterWorld.L08_le_principal_iff
 
 World "FilterWorld"
 Level 9
@@ -6,7 +6,9 @@ Level 9
 Title "Principal filters and `≤`."
 
 Introduction "Let's prove that `≤` on filters \"extends\" `⊆` on sets. In other words,
-our definition of `≤` for generalized sets agrees with the usual notion of `⊆` on sets."
+our definition of `≤` for generalized sets agrees with the usual notion of `⊆` on sets.
+
+A hint: you don't have to use `constructor` to solve this level."
 
 namespace MyGame
 
@@ -22,21 +24,20 @@ TheoremDoc MyGame.principal_mono as "principal_mono" in "Filter"
 -- **TODO** needs human-readable proof, or explanation of tauto?
 /--  If `A` and `B` are subsets of `𝓧` then `𝓟 A ≤ 𝓟 B ↔ A ⊆ B` . -/
 Statement principal_mono {A B : Set 𝓧} : 𝓟 A ≤ 𝓟 B ↔ A ⊆ B := by
-constructor
-rw[le_def] at *
-intro h
-specialize h A
-rw [mem_principal] at h
-rw [mem_principal] at h
-intro x
-rw [subset_def] at h
-specialize h 
-sorry
-sorry
+  rw [le_principal_iff]
+  rw [mem_principal]
 
+Conclusion "A two-line proof:
+```
+  rw [le_principal_iff]
+  rw [mem_principal]
+```
 
-  
-  
+The goal is closed at the end because it becomes `A ⊆ B ↔ A ⊆ B`, which
+can be proved with `rfl` as `↔` is reflexive, and `rw` tries `rfl` after
+each usage to see if it works.
 
-Conclusion "The final thing we'll do this in world is to prove that if `𝓟 A = 𝓟 B` then `A = B`.
+The final thing we'll do this in world is to prove that if `𝓟 A = 𝓟 B` then `A = B`.
 In other words, we'll show that two distinct sets give us distinct filters."
+
+end MyGame
