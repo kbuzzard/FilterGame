6ff3bee053d5c58d0e08a01e14aa73862d057079
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2024-07-29 18:43:41 +0100

    filter level 10

--- Game/Levels/FilterWorld/L10_principal_eq_iff_eq.lean
+++ Game/Levels/FilterWorld/L10_principal_eq_iff_eq.lean
@@ -22,7 +22,30 @@ TheoremDoc MyGame.principal_mono as "principal_mono" in "Filter"
 -- **TODO** needs human-readable proof, or explanation of tauto?
 /--  If `A` and `B` are subsets of `𝓧` then `𝓟 A = 𝓟 B ↔ A = B`. -/
 Statement principal_eq_iff_eq {A B : Set 𝓧} : 𝓟 A = 𝓟 B ↔ A = B := by
-  sorry -- use antisymm and previous level and ext and hopefully that's it
+  constructor
+  · intro h
+    apply subset_antisymm
+    · rw [← principal_mono, h]
+    · rw [← principal_mono, h]
+  · intro h
+    rw [h]
 
-Conclusion "Congratulations, you've finished the first filter world! If you've already
-done function world then you can go on to pushing and pulling filters."
+Conclusion "Here's my proof:
+```
+  constructor
+  · intro h
+    apply subset_antisymm
+    · rw [← principal_mono, h]
+    · rw [← principal_mono, h]
+  · intro h
+    rw [h]
+```
+You don't need to type the dots, they're just to indicate when one goal splits into two.
+
+Congratulations, you've finished the first filter world! If you've already
+done function world then you can go on to pushing and pulling filters, apart from the
+fact that right now both function world and the pushing/pulling filter world are not
+made yet. Check out `https://github.com/kbuzzard/FilterGame/blob/main/Game/LMSfiltergame.lean`
+(search for \"# Function World\" and \"# Pushing forward and pulling back filters\") to
+see the levels which should go into this world, and feel free to make a pull request
+if you want to add some new levels!""
